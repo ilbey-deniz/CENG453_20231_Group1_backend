6103a9f7bbc2e38e053a0bbc04c8af8fd06f1985
ilbey-deniz <mustafa.ilbey@hotmail.com>
2023-10-21 11:06:01 +0300

    initial commit
    Deployed the sample customers app

---
+++ src/main/java/com/group1/backend/customer/Customer.java
@@ -0,0 +1,12 @@
+package com.group1.backend.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
