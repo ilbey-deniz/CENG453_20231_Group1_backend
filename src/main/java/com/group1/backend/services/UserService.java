3254db944280ca5ef64897e38bc4bd98bf213a10
Fatih <m.fatih.berberoglu@gmail.com>
2023-11-09 00:15:36 +0300

    add IsUserExistByName and saveRepository methods to UserService

--- src/main/java/com/group1/backend/services/UserService.java
+++ src/main/java/com/group1/backend/services/UserService.java
@@ -2,16 +2,14 @@ package com.group1.backend.services;
 
 import com.group1.backend.entities.UserEntity;
 import com.group1.backend.repositories.UserRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
-
-
 
     private final UserRepository userRepository;
 
@@ -23,6 +21,19 @@ public class UserService {
 
     public List<UserEntity> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public boolean IsUserExistByName(String userName){
+        return this.userRepository.existsByName(userName);
+    }
+
+    public String getRoleByName(String userName){
+        Optional<UserEntity> user = this.userRepository.findByName(userName);
+        return user.orElseThrow().getRole();
+    }
+
+    public void saveRepository(UserEntity user){
+        this.userRepository.save(user);
     }
 
 }
