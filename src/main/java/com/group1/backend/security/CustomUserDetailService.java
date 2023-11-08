71edde75dd5c783fe31550321ec66e83c37fd83b
Fatih <m.fatih.berberoglu@gmail.com>
2023-11-09 00:13:56 +0300

    implement userDetailService

---
+++ src/main/java/com/group1/backend/security/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package com.group1.backend.security;
+
+import com.group1.backend.entities.UserEntity;
+import com.group1.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> user = userRepository.findByName(username);
+        if (user.isEmpty()){
+            throw new UsernameNotFoundException("Username not found!");
+        }
+        // TODO: add roles later
+        return new User(user.get().getName(), user.get().getPassword(), new ArrayList<>());
+    }
+}
