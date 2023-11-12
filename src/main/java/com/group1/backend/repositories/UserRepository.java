310ad8adb333abc07ffb1c2335b88bd525d9f21a
Fatih <m.fatih.berberoglu@gmail.com>
2023-11-12 23:24:14 +0300

    add some queries for testing

--- src/main/java/com/group1/backend/repositories/UserRepository.java
+++ src/main/java/com/group1/backend/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.group1.backend.repositories;
 
 import com.group1.backend.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,16 @@ public interface UserRepository extends JpaRepository <UserEntity, Integer> {
     boolean existsByEmail(String email);
 
     Optional<UserEntity> findByEmail(String email);
+
+    @Query(value = """
+            select U.name
+             from user U
+             where U.name like '%TestUser%'
+             order by length(U.name) desc, U.name desc\s
+             limit 1
+             """,
+            nativeQuery = true)
+        Collection<Object> findLastTestUser();
+
+    void deleteByName(String userName);
 }
